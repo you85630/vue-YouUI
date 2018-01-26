2220abc92d90df784009f3105532c9593e654cf8
You <you85630@foxmail.com>
2018-01-26 16:25:28 +0800

    init

--- src/components/modules/select.vue
+++ src/components/modules/select.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="select">
     <div class="input">
-      <p @click="changInput" :class="{active:show}" v-if="!select">请选择</p>
-      <p @click="changInput" class="now" :class="{active:show}" v-else>{{select}}</p>
+      <p @click="changInput" :class="{active:show}">
+        <span v-if="!select">请选择</span>
+        <span class="now" v-else>{{select}}</span>
+        <i class="fa" :class="show?'fa-angle-up':'fa-angle-down'"></i>
+      </p>
     </div>
     <ul v-if="show">
       <li v-for="li in list" :key="li.index" @click="option(li)">{{li}}</li>
@@ -41,26 +44,40 @@ export default {
     p{
       display: inline-block;
       box-sizing: border-box;
-      min-width: 200px;
+      width: 100px;
       padding: 6px 15px;
-      font-size: 12px;
       border-radius: 2px;
       border: 1px solid #ccc;
-      color: #ccc;
+      font-size: 0;
       &:hover{
         border-color: #409eff;
+      }
+      span{
+        color: #ccc;
+        font-size: 12px;
+        width: 80%;
+        display: inline-block;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+      .now{
+        color: #000;
+      }
+      i{
+        font-size: 14px;
+        width: 20%;
+        text-align: right;
+        vertical-align: top;
       }
     }
     .active{
       border-color: #409eff;
     }
-    .now{
-      color: #000;
-    }
   }
   ul{
     position: absolute;
-    min-width: 200px;
+    width: 100px;
     left: 0;
     top: 30px;
     z-index: 20;

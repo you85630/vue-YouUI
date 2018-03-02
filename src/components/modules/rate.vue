d40212d0d67cc7aef40ba0e3f2c47feed1888243
You <you85630@foxmail.com>
2018-03-02 11:21:08 +0800

    完善评分

--- src/components/modules/rate.vue
+++ src/components/modules/rate.vue
@@ -1,32 +1,49 @@
 <template>
   <div class="rate">
-    <i class="fa" v-for="li in ilist" :key="li.index" @mousemove="hover(li)" :class="{'fa-star':li.active,'fa-star-o':!li.active}"></i>
+    <i class="fa" v-for="(li,index) in ilist" :key="li.index" @mouseover="stars(index)" :class="li"></i>
   </div>
 </template>
 
 <script>
 export default {
+  props: ['value'],
   data () {
     return {
-      ilist: [
-        {
-          active: false
-        }, {
-          active: false
-        }, {
-          active: false
-        }, {
-          active: false
-        }, {
-          active: false
-        }
-      ]
+      star: this.value
     }
   },
   methods: {
-    hover (i) {
-      i.active = !i.active
+    stars (key) {
+      this.star = key + 1
+      this.$emit('input', this.star)
     }
+  },
+  computed: {
+    ilist () {
+      if (this.star > 5) {
+        this.star = 5
+      } else {
+        this.star = this.star
+      }
+      let list = []
+      let now = Math.floor(this.star * 2) / 2
+      let hasDecimal = now % 1 !== 0
+      let integer = Math.floor(now)
+      for (let i = 0; i < integer; i++) {
+        list.push('fa-star')
+      }
+      if (hasDecimal) {
+        list.push('fa-star-half-o')
+      }
+      while (list.length < 5) {
+        list.push('fa-star-o')
+      }
+      this.$emit('input', this.star)
+      return list
+    }
+  },
+  created () {
+    this.$emit('input', this.star - 1)
   }
 }
 </script>
--- src/components/psges/rate.vue
+++ src/components/psges/rate.vue
@@ -1,17 +1,42 @@
 <template>
   <div>
-    <my-rate></my-rate>
+    <my-rate v-model="star"></my-rate>
+    <p>{{star}}</p>
+
+    <my-tips :list="tableList"></my-tips>
   </div>
 </template>
 
 <script>
 import myRate from 'components/modules/rate'
+import myTips from 'components/common/tips'
 export default {
   components: {
-    myRate
+    myRate,
+    myTips
+  },
+  data () {
+    return {
+      star: 3,
+      tableList: [
+        {
+          name: 'value',
+          explain: '指定当前星星的数据。可以使用 v-model 双向绑定数据',
+          types: 'Number',
+          default: '-'
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+p{
+  font-size: 14px;
+  margin-top: 10px;
+}
+.tips{
+  margin-top: 20px;
+}
 </style>

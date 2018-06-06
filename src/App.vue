17b0ef0610db4e66a41e59debf1a900643b27128
You <you85630@foxmail.com>
2018-06-06 15:27:30 +0800

    增加跳转动画

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <router-view/>
+    <transition name="slide-fade" mode="out-in">
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
+    </transition>
   </div>
 </template>
 
@@ -8,5 +12,19 @@
 #app {
   height: 100%;
   font-size: 0;
+}
+
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+
+.slide-fade-leave-active {
+  transition: all .5s ease-in-out;
+}
+
+.slide-fade-enter,
+.slide-fade-leave-active {
+  opacity: 0;
+  transform: translateX(10px);
 }
 </style>
